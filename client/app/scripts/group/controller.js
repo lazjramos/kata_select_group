785d8bc0b8f5bd0b58c384cef51342c9d3ace53c
Laz J. Ramos <lazjramos@gmail.com>
2017-07-01 15:00:18 -0500

    Hecho hasta ahora en la oficina

--- client/app/scripts/group/controller.js
+++ client/app/scripts/group/controller.js
@@ -1,15 +1,95 @@
 'use strict';
 
 angular.module('Group')
-.controller('group', function ($scope) {
+    .controller('group', function($scope) {
 
-  $scope.controller_loaded = 'Group loaded!';
+        $scope.controller_loaded = 'Group loaded!';
 
-})
-.config(function ($routeProvider) {
-  $routeProvider
-  .when('/group', {
-    templateUrl: 'scripts/group/views/group.html',
-    controller: 'group'
-  });
-});
+        $scope.suma = function(num1, num2) {
+            return num1 + num2;
+        };
+
+        $scope.todosEquipos = function(inputs) {
+            var estocolmo = [];
+            var londres = [];
+            var proyectosEstocolmo = [];
+            var proyectosLondres = [];
+
+            inputs.forEach(function(element) {
+                if (estocolmo.length === 0) {
+                    estocolmo.push({ numero: element[0] });
+                } else {
+                    var flagEstocolmo = 0;
+                    estocolmo.forEach(function(individuoEstocolmo) {
+                        if (individuoEstocolmo.numero === element[0]) {
+                            flagEstocolmo++;
+                        }
+
+                        if (flagEstocolmo === 0) {
+                            estocolmo.push({ numero: element[0] });
+                        }
+                    });
+                }
+
+                if (londres.length === 0) {
+                    londres.push({ numero: element[1] });
+                } else {
+                    var flagLondres = 0;
+                    londres.forEach(function(individuoLondres) {
+                        if (individuoLondres.numero === element[1]) {
+                            flagLondres++;
+                        }
+
+                        if (flagLondres === 0) {
+                            londres.push({ numero: element[1] });
+                        }
+                    });
+                }
+
+                proyectosEstocolmo.push(element[0]);
+                proyectosLondres.push(element[1]);
+            });
+
+            estocolmo.forEach(function(progEstocolmo) {
+                progEstocolmo.cantidad = 0;
+                proyectosEstocolmo.forEach(function(proyectoEstocolmo) {
+                    if (progEstocolmo.numero === proyectoEstocolmo) {
+                        progEstocolmo.cantidad++;
+                    }
+                });
+            });
+
+            londres.forEach(function(progLondres) {
+                progLondres.cantidad = 0;
+                proyectosLondres.forEach(function(proyectoLondres) {
+                    if (progLondres.numero === proyectoLondres) {
+                        progLondres.cantidad++;
+                    }
+                });
+            });
+
+
+            inputs.forEach(function(proyecto) {
+                var candidatoEstocolmo;
+                // var candidatoLondres;
+
+                console.log('proyecto' + proyecto);
+                estocolmo.forEach(function(est) {
+                    if (proyecto[0] === est.numero) {
+                        candidatoEstocolmo = est;
+                    }
+                    console.log('candidato estocolmo  ' + candidatoEstocolmo.numero + ' ' + candidatoEstocolmo.cantidad);
+                });
+            });
+
+            return 'ok';
+        };
+
+    })
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/group', {
+                templateUrl: 'scripts/group/views/group.html',
+                controller: 'group'
+            });
+    });
--- client/app/scripts/group/specs/controller_spec.js
+++ client/app/scripts/group/specs/controller_spec.js
@@ -1,51 +1,72 @@
 'use strict';
 
-describe('Controller: select group', function () {
+describe('Controller: select group', function() {
 
-  beforeEach(module('Group'));
+    beforeEach(module('Group'));
 
-  var controller;
-  var scope;
+    var controller;
+    var scope;
 
-  beforeEach(inject(function ($rootScope, $controller) {
-    scope = $rootScope.$new();
-    controller = $controller('group', { $scope: scope });
-  }));
-
-  describe('On instance', function () {
-    it('should set "controller_loaded" variable in scope', function () {
-      expect(scope.controller_loaded).toContain('loaded');
-    });
-  });
-
-  describe('when going to /group', function () {
-
-    var route, location, rootScope, httpBackend;
-
-    beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
-      route = $route;
-      location = $location;
-      rootScope = $rootScope;
-      httpBackend = $httpBackend;
-
-      httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+    beforeEach(inject(function($rootScope, $controller) {
+        scope = $rootScope.$new();
+        controller = $controller('group', { $scope: scope });
     }));
 
-    afterEach(function () {
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
+    describe('On instance', function() {
+        it('should set "controller_loaded" variable in scope', function() {
+            expect(scope.controller_loaded).toContain('loaded');
+        });
+
+        it('test sum function', function() {
+            expect(scope.suma(1, 2)).toEqual(3);
+            expect(scope.suma(2, 2)).toEqual(4);
+        });
+
+        it('todos equipos', function() {
+            expect(scope.todosEquipos([
+                [1000, 2000],
+                [1001, 2000],
+                [1000, 2002]
+            ])).toEqual([1000, 2000]);
+        });
+
+        iit('todos equipos 2', function() {
+            expect(scope.todosEquipos([
+                [1020, 2003],
+                [1023, 2003],
+                [1020, 2001]
+            ])).toEqual([1020, 2003]);
+        });
     });
 
-    it('should use minesweeper.html and controller', function () {
-      expect(route.current).toBeUndefined();
+    describe('when going to /group', function() {
 
-      location.path('/group');
+        var route, location, rootScope, httpBackend;
 
-      httpBackend.flush();
+        beforeEach(inject(function($route, $location, $rootScope, $httpBackend) {
+            route = $route;
+            location = $location;
+            rootScope = $rootScope;
+            httpBackend = $httpBackend;
 
-      expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
-      expect(route.current.controller).toBe('group');
+            httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+        }));
+
+        afterEach(function() {
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should use minesweeper.html and controller', function() {
+            expect(route.current).toBeUndefined();
+
+            location.path('/group');
+
+            httpBackend.flush();
+
+            expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
+            expect(route.current.controller).toBe('group');
+        });
     });
-  });
 
 });
